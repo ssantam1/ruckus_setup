6be4f72fc1920e2fb96431ab283691be99207d34
steven santamorena <ssantam1@binghamton.edu>
2025-01-02 13:54:01 -0500

    Refactor serial communication into a Connection class

--- main.py
+++ main.py
@@ -2,52 +2,56 @@ import serial
 import time
 import config
 
-def send_command(ser: serial.Serial, command: str, sleep=1):
-    ser.write((command + '\n').encode())
-    time.sleep(sleep)
-    response = ser.read_all().decode()
-    return response
+class Connection:
+    def __init__(self, port):
+        self.ser = serial.Serial(
+            port=port,
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
 
-def send_command_with_wait(ser: serial.Serial, command, wait_for=None):
-    response = send_command(ser, command)
-    if wait_for:
-        print(response, end='')
-        while wait_for not in response:
-            time.sleep(1)
-            response = ser.read_all().decode()
-    return response
+    def send_command(self, command: str, sleep=1):
+        self.ser.write((command + '\n').encode())
+        time.sleep(sleep)
+        response = self.ser.read_all().decode()
+        return response
+
+
+    def send_command_with_wait(self, command: str, wait_for=None):
+        response = self.send_command(command)
+        if wait_for:
+            print(response, end='')
+            while wait_for not in response:
+                time.sleep(1)
+                response = self.ser.read_all().decode()
+        return response
+    
+    def close(self):
+        self.ser.close()
 
 def main():
-    ser = serial.Serial(
-        port=input('Enter COM port: '),
-        baudrate=9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-    )
-
-    if ser.is_open:
-        print("Serial connection established.")
-    else:
-        print("Failed to establish serial connection.")
-        return
+    port = input("Enter the COM port: ")
+    conn = Connection(port)
 
     commands = config.commands
 
     # Send a blank input to find out if we need to login or not
-    response = send_command(ser, '')
+    response = conn.send_command('')
     print(response, end='')
+
 
     # Send commands and print output as if you were doing it manually
     for command in commands:
         if isinstance(command, tuple):
-            response = send_command_with_wait(ser, command[0], wait_for=command[1])
+            response = conn.send_command_with_wait(command[0], wait_for=command[1])
         else:
-            response = send_command(ser, command)
+            response = conn.send_command(command)
         print(response, end='')
 
-    ser.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
