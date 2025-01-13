9e82c1443e03f822e440298d321ab6aafb8fac4f
steven santamorena <ssantam1@binghamton.edu>
2025-01-13 17:45:34 -0500

    Modify example config

--- config.example.py
+++ config.example.py
@@ -10,33 +10,53 @@ password = 'admin'
 
 # Configuration commands
 commands = [
-    {
+    {  # Enable privileged mode
         'command': 'enable',
         'validators': ['#'],  # Expect privileged prompt
     },
-    {
+    {  # Enter config mode
         'command': 'conf t',
         'validators': ['(config)#'],  # Expect config prompt
     },
-    {
+    {  # Set hostname
         'command': f'hostname {hostname}',
         'validators': [f'{hostname}(config)#'],
     },
-    {
+    {  # Set IP address and subnet mask
         'command': f'ip address {ip_address} {subnet_mask}',
-        'validators': ['Interface IP address is set'],
     },
-    {
+    {  # Set default gateway
         'command': f'ip default-gateway {default_gateway}',
-        'validators': ['Default gateway is set'],
     },
-    {
+    {  # Generate SSL certificate
         'command': 'crypto-ssl certificate generate',
         'wait_for': 'ssl-certificate creation is successful',  # Don't close connection until this is seen
         'validators': ['Creating certificate, please wait'],
     },
-    {
-        'command': 'exit',
-        'validators': ['#'],  # Expect privileged prompt
+    {  # Create user for management
+        'command': f'username {username} password {password}',
+        'validators': ['User created successfully'],
     },
+    {  # Generate RSA key pair
+        'command': 'crypto key generate rsa',
+        'validators': ['Key already exists. Please zeroize it', 'Creating RSA key pair, please wait'],
+    },
+    {  # Set timezone
+        'command': 'clock timezone us Eastern',
+    },
+    {  # Enter interface configuration mode
+        'command': 'int e1/1/1 to 1/1/24',
+        'validators': ['(config-mif-1/1/1-1/1/24)#'],
+    },
+    {  # Set inline power priority
+        'command': 'inline power priority 1 power-by-class 2\n',
+    },
+    {  # Return to config mode
+        'command': 'exit',
+        'validators': ['(config)#'],
+    },
+    {  # Return to privileged mode
+        'command': 'exit',
+        'validators': ['#'],
+    }
 ]
